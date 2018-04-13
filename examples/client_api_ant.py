ce91ebd1dacedb7277690ea005184eacfe446a1f
1nir1 <kravnir@gmail.com>
2018-04-13 17:47:43 +0300

    client examples update

--- examples/client_api_ant.py
+++ examples/client_api_ant.py
@@ -4,6 +4,9 @@ import pba
 import math
 import time
 
+'''
+Example for building ANT module using the Robot_Module API.
+'''
 def buildAnt():
     w = pba.Robot_Module()
 
@@ -17,64 +20,70 @@ def buildAnt():
     w.addBody("aux_1","0.2 0.2 0")
     w.addJoint("hip_1","0 0 0","0 0 1","-30 30","150")
     w.addGeom("left_leg_geom","0 0 0 0.2 0.2 0","0.08","capsule")
-    w.addBody("shit1","0.2 0.2 0")
+    w.addBody("mid1","0.2 0.2 0")
     w.addJoint("ankle_1","0 0 0","-1 1 0","30 70","150")
     w.addGeom("left_ankle_geom","0 0 0 0.4 0.4 0","0.08","capsule")
+
     # Front right leg
     w.addBody("front_right_leg","0 0 0","torso")
     w.addGeom("aux_2_geom","0.0 0.0 0.0 -0.2 0.2 0","0.08","capsule")
     w.addBody("aux_2","-0.2 0.2 0")
     w.addJoint("hip_2","0 0 0","0 0 1","-30 30","150")
     w.addGeom("right_leg_geom","0 0 0 -0.2 0.2 0","0.08","capsule")
-    w.addBody("shit2","-0.2 0.2 0")
+    w.addBody("mid2","-0.2 0.2 0")
     w.addJoint("ankle_2","0 0 0","1 1 0","-70 -30","150")
     w.addGeom("right_ankle_geom","0 0 0 -0.4 0.4 0","0.08","capsule")
+
     # Left back leg
     w.addBody("back_leg","0 0 0","torso")
     w.addGeom("aux_3_geom","0.0 0.0 0.0 -0.2 -0.2 0","0.08","capsule")
     w.addBody("aux_3","-0.2 -0.2 0")
     w.addJoint("hip_3","0 0 0","0 0 1","-30 30","150")
     w.addGeom("back_leg_geom","0 0 0 -0.2 -0.2 0","0.08","capsule")
-    w.addBody("shit3","-0.2 -0.2 0")
+    w.addBody("mid3","-0.2 -0.2 0")
     w.addJoint("ankle_3","0 0 0","-1 1 0","-70 -30","150")
     w.addGeom("third_ankle_geom","0 0 0 -0.4 -0.4 0","0.08","capsule")
+
     # Right back leg
     w.addBody("right_back_leg","0 0 0","torso")
     w.addGeom("aux_4_geom","0.0 0.0 0.0 0.2 -0.2 0","0.08","capsule")
     w.addBody("aux_4","0.2 -0.2 0")
     w.addJoint("hip_4","0 0 0","0 0 1","-30 30","150")
     w.addGeom("right_back_leg_geom","0 0 0 0.2 -0.2 0","0.08","capsule")
-    w.addBody("shit4","0.2 -0.2 0")
+    w.addBody("mid4","0.2 -0.2 0")
     w.addJoint("ankle_4","0 0 0","1 1 0","30 70","150")
     w.addGeom("fourth_ankle_geom","0 0 0 0.4 -0.4 0","0.08","capsule")
 
     return w
+
+'''
+Example of basic ANT module movement given a built Robot_Module 'w'
+'''
 def moveAnt(w):
-    w.startRun('our_ant')
+    w.startRun('our_ant') # default visual = True => visual simulation will appear
 
     angular_velocity = 0.1
     place = math.pi / 4
 
-    for i in range(100):
+    # Let the robot fall down - affected only by gravity.
+    for _ in range(100):
         w.makeStep()
     time.sleep(2)
 
-    #Stand up
-    for j in range(5):
-        if j == 0: continue
+    print("Stand up")
+    for j in [1, 4, 2, 3]:
         for i in range(1):
             if i == 1:
                 print(str(j))
             angle = 50
             if j == 2 or j == 3:
                 angle = -angle
-            w.moveJoint("ankle_" + str(j), angle, 0 * angular_velocity)
+            w.moveJoint("ankle_" + str(j), angle, angular_velocity)
             for _ in range(10):
                 w.makeStep()   
                         
-    print ("Try move")
+    print ("Start movement")
     for j in [1, 4, 2, 3]:
-        if j == 0: continue
         for i in range(100):
             if i == 1:
                 print(str(j))
@@ -108,9 +117,12 @@ def moveAnt(w):
         w.moveJoint("ankle_2", place*1.5, angular_velocity)
         w.makeStep()
 
-    while 1:
-        w.makeStep()
+    # makeStep returns a list of joint that didn't finish the requested move yet - when the list is empty - finish the run.
+    while len(w.makeStep()) != 0:
+        continue
     
+    print("End movement")
     w.stopRun()
+
 if __name__=="__main__":
     moveAnt(buildAnt())
--- examples/client_api_walker.py
+++ examples/client_api_walker.py
@@ -4,63 +4,79 @@ import pba
 import math
 import time
 
+'''
+Example for building WALKER module using the Robot_Module API.
+'''
 def buildWalker():
     w = pba.Robot_Module()
+
+    # Main body
     w.addBody("torso", "0 0 1.5")
     w.addGeom("torso_geom", "0 0 1.45 0 0 1.05", "0.12", "capsule", "torso")
 
+    # Right leg
     w.addBody("tie", "0 0.2 0", "torso")
-    w.addJoint("tie_joint", "1", "0 -1 0", "-150 0", "200", "tie")
-    w.addGeom("tie_geom", "0 0 1.05 0 0 0.6", "0.05", "capsule", "tie")
+    w.addJoint("tie_joint", "1", "0 -1 0", "-150 0", "200")
+    w.addGeom("tie_geom", "0 0 1.05 0 0 0.6", "0.05", "capsule")
 
     w.addBody("leg", "0 0 0")
-    w.addJoint("leg_joint", "1", "0 -1 0", "-150 0", "200", "leg")
-    w.addGeom("leg_geom", "0 0 0.6 0 0 0.1", "0.04", "capsule", "leg")
+    w.addJoint("leg_joint", "1", "0 -1 0", "-150 0", "200")
+    w.addGeom("leg_geom", "0 0 0.6 0 0 0.1", "0.04", "capsule")
     
-    w.addBody("foot", "0 0 0", "leg")
-    w.addJoint("foot_joint", "1", "0 -1 0", "-45 45", "200", "foot")
-    w.addGeom("foot_geom", "-0.13 0 0.1 0.26 0 0.1", "0.06", "capsule", "foot")
+    w.addBody("foot", "0 0 0")
+    w.addJoint("foot_joint", "1", "0 -1 0", "-45 45", "200")
+    w.addGeom("foot_geom", "-0.13 0 0.1 0.26 0 0.1", "0.06", "capsule")
     
+    # Left leg
     w.addBody("tie_left", "0 -0.2 0", "torso")
-    w.addJoint("tie_joint_left", "1", "0 -1 0", "-150 0",  "200", "tie_left")
-    w.addGeom("tie_geom_left", "0 0 1.05 0 0 0.6", "0.05", "capsule", "tie_left")
+    w.addJoint("tie_joint_left", "1", "0 -1 0", "-150 0",  "200")
+    w.addGeom("tie_geom_left", "0 0 1.05 0 0 0.6", "0.05", "capsule")
 
-    w.addBody("leg_left", "0 0 0", "tie_left")
-    w.addJoint("leg_joint_left", "1", "0 -1 0", "-150 0",  "200", "leg_left")
-    w.addGeom("leg_geom_left", "0 0 0.6 0 0 0.1", "0.04", "capsule", "leg_left")
+    w.addBody("leg_left", "0 0 0")
+    w.addJoint("leg_joint_left", "1", "0 -1 0", "-150 0",  "200")
+    w.addGeom("leg_geom_left", "0 0 0.6 0 0 0.1", "0.04", "capsule")
     
-    w.addBody("foot_left", "0 0 0", "leg_left")
-    w.addJoint("foot_joint_left", "1", "0 -1 0", "-45 45", "200", "foot_left")
-    w.addGeom("foot_geom_left", "-0.13 0 0.1 0.26 0 0.1", "0.06", "capsule", "foot_left")
+    w.addBody("foot_left", "0 0 0")
+    w.addJoint("foot_joint_left", "1", "0 -1 0", "-45 45", "200")
+    w.addGeom("foot_geom_left", "-0.13 0 0.1 0.26 0 0.1", "0.06", "capsule")
 
     return w
+    
+'''
+Example of basic WALKER module movement given a built Robot_Module 'w'
+'''
 def walkOurWalker(w):
-    w = buildWalker()
-    w.startRun('our_hopper', visual=False)
-    flag = True
-    while (1):
+    w.startRun('our_walker', visual=False) # visual = False => only console.
+    for _ in range(1000):
         half = 40
         deg = 30
         if (w.getFrame() % half < half * 0.5):
-            if flag:
-                w.moveJoint("foot_joint", deg , 1)
-                w.moveJoint("leg_joint", deg / 4, 1)
-                w.moveJoint("foot_joint_left", 0, 1)
-                w.moveJoint("leg_joint_left",0, 1)
-                w.moveJoint("tie_joint_left",0, 1)
-                w.moveJoint("tie_joint", deg, 1)
+            # Move right foot
+            w.moveJoint("tie_joint", deg, 1)
+            w.moveJoint("foot_joint", deg , 1)
+            w.moveJoint("leg_joint", deg / 4, 1)
+
+            w.moveJoint("tie_joint_left",0, 1)
+            w.moveJoint("foot_joint_left", 0, 1)
+            w.moveJoint("leg_joint_left",0, 1)
         else:
-            flag = False
-            w.moveJoint("foot_joint", deg * 3, 1)
+            # Move left foot
+            w.moveJoint("foot_joint", 0, 1)
             w.moveJoint("leg_joint", 0, 1)
             w.moveJoint("tie_joint", 0, 1)
-            w.moveJoint("foot_joint_left",0, 1)
-            w.moveJoint("leg_joint_left", 0, 1)
-            w.moveJoint("tie_joint_left", 0, 1)
-        #for _ in range(100):
-        w.makeStep()
+            
+            w.moveJoint("tie_joint_left", deg, 1)
+            w.moveJoint("foot_joint_left",deg, 1)
+            w.moveJoint("leg_joint_left", deg / 4, 1)
+
+        # makeStep returns a list of joint that didn't finish the requested move yet - when the list is empty - finish the run.
+        while len(w.makeStep()) != 0:
+            continue
+
         pos, ori = w.getPositionAndOrientation()
         print (str(pos))
+
+    w.stopRun()
 
 if __name__=="__main__":
     walkOurWalker(buildWalker())
